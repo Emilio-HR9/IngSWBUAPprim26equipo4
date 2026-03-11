608aa951283a1514500b15a82ef28d83ebfc6544
Habido15 <habid150505om@gmail.com>
2026-03-10 18:20:43 -0600

    Fix video autoplay muted issue and resolve UI state change detection bug

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// 1. Importa el componente que creaste
+import { Modulo4PresentacionComponent } from './componentes/modulo4-presentacion/modulo4-presentacion';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  // 2. Agrégalo aquí a la lista de imports
+  imports: [RouterOutlet, Modulo4PresentacionComponent], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('modulo4-frontend');
+export class AppComponent {
+  title = 'modulo4-frontend';
 }
